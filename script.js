bc5b9772d74e0353cfbef71b4fca146b95500d0f
nkricos <122754869+nkricos@users.noreply.github.com>
2023-05-24 16:05:21 -0400

    create js loops to generate 4x4 grid of flex divs

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 
 //create a div box in javascript and display in div #container
 const theBox = document.createElement('div');
-theBox.setAttribute('id', 'box');
+theBox.setAttribute('class', 'box');
+
 const theGrid = document.querySelector('#container');
 
 
 //create a flex row to add boxes to
 const theRow = document.createElement('div');
-theRow.setAttribute('id', 'flexRow');
+theRow.setAttribute('class', 'flexRow');
 
-theGrid.appendChild(theRow);
-theRow.appendChild(theBox);
+
+
 
 //loop to create four rows
 //populate each row with four boxes
+
+for (let i = 0; i < 4; i++){
+    theRow.appendChild(theBox.cloneNode(true));
+}
+
+for (let i = 0; i < 4; i++) {
+    theGrid.appendChild(theRow.cloneNode(true));
+}
